30df8a35ecbed4bcac52bfedb2279a45d4e2a270
Kaustubh <pro.vaidya4413@gmail.com>
2023-12-24 21:14:00 +0530

    implemented geolocator and flutter_sms package, draft sms can be send using by the onclick event

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,33 +11,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    String message = "This is a sample distress message from Kaustubh";
+
+    List<String> recipents = ["9922450488", "8080223251"];
+
+    void _sendSMS(String message, List<String> recipents) async {
+      String _result = await sendSMS(message: message, recipients: recipents)
+          .catchError((onError) {
+        return onError.toString();
+      });
+      print(_result);
+    }
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ClipOval(
-          child: Material(
-            color: Colors.red,
-            child: InkWell(
-              splashColor: Colors.white,
-              //TODO: Add onTap for the button
-              onTap: () {debugPrint("Button tapped");},
-              child: const SizedBox(
-                width: 200,
-                height: 200,
-                child: Center(
-                  child: Text(
-                    "ResQConnect",
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white
+        backgroundColor: Colors.white,
+        body: Center(
+          child: ClipOval(
+            child: Material(
+              color: Colors.red,
+              child: InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {
+                    _sendSMS(message, recipents);
+                  },
+                  child: const SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        "ResQConnect",
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
                     ),
-                  ),
-                ),
-              )
+                  )),
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
